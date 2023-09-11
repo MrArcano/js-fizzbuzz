1a391ecdc4d879ceedcea05eb0d0c884b9b5c3b0
MrArcano <salvatore-dario@hotmail.it>
2023-09-11 13:29:31 +0200

    [add]: file script

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+- stampo i numeri da 1 a 100
+
+- se il numero è multiplo di 3 - stampo "Fizz"
+- se il numero è multiplo di 5 - stampo "Buzz"
+- se il numero è multiplo di 3 e 5 - stampo "FizzBuzz"
+
+*/
