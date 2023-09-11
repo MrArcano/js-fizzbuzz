3f788977eda458998aa49d7fc43ea1f902d9cace
MrArcano <salvatore-dario@hotmail.it>
2023-09-11 13:34:00 +0200

    [edit]: scirpt fizz buzz

--- js/script.js
+++ js/script.js
@@ -4,5 +4,19 @@
 - se il numero è multiplo di 3 - stampo "Fizz"
 - se il numero è multiplo di 5 - stampo "Buzz"
 - se il numero è multiplo di 3 e 5 - stampo "FizzBuzz"
-
 */
+
+
+for(let i=1; i<=100; i++){
+  console.log(i);
+  if (!(i%3)){
+    console.log("Fizz")
+  }
+  if (!(i%5)){
+    console.log("Buzz")
+  }
+  if ((!(i%3)) && (!(i%5))){
+    console.log("FizzBuzz")
+  }
+  
+}
