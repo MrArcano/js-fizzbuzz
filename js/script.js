d0b355ee07d38f31a45ba208b72118ec227b1ef4
MrArcano <salvatore-dario@hotmail.it>
2023-09-11 14:56:09 +0200

    [add]: style FizzBuzz

--- js/script.js
+++ js/script.js
@@ -6,21 +6,36 @@
 - se il numero è multiplo di 3 e 5 - stampo "FizzBuzz"
 */
 let text = "";
+const containerBox = document.querySelector(".container-box")
 
 for(let i=1; i<=100; i++){
-  
+
+  const box = document.createElement("div");
+  box.classList.add("box");
+
+  // testo di default
   text = i;
 
+  // testo se multiplo di 3
   if (!(i%3)){
     text = "Fizz";
+    box.classList.add("bg-fizz");
   }
+
+  // testo se multiplo di 5
   if (!(i%5)){
     text = "Buzz";
+    box.classList.add("bg-buzz");
   }
+
+  // testo se multiplo di 3 e 5
   if ((!(i%3)) && (!(i%5))){
     text = "FizzBuzz";
+    box.classList.add("bg-fizzbuzz");
   }
-  
-  console.log(text);
 
+
+  box.append(text);
+  containerBox.append(box);
 }
+
